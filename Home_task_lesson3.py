5c02f89de4df80faecf8141d7dfe6835d9f6a971
Ina Haurylava <morqueshion789@mail.ru>
2024-11-11 23:24:12 +0300

    Changes for Lesson 3

--- Home_task_lesson3.py
+++ Home_task_lesson3.py
@@ -17,9 +17,10 @@ new_word = 'ing'
 print(word + new_word)
 
 # 4. В строке “Ivanou Ivan” поменяйте местами слова => "Ivan Ivanou"
-name = [ "Ivanov" , "Ivan" ]
-name.reverse()
-print(f"{name[0]} {name[1]}")
+name = "Ivanou Ivan"
+words = name.split()
+reversed_name = " ".join(reversed(words))
+print(reversed_name)
 
 # 5. Напишите программу которая удаляет пробел в начале, в конце строки
 stroka = "   Hello, Minsk      "
@@ -28,7 +29,7 @@ print(stroka)
 print(new_stroka)
 
 # 6. Создайте словарь, связав его с переменной school, и наполните его данными которые бы отражали количество учащихся в десяти разных классах (например, 1а, 1б, 2б, 6а, 7в и т.д.).
-school = {"1a": 15, "1b" : 18, "1c": 21, "2a" : 25, "2b": 17, "2c" : 19, "3a": 22, "3b" : 26, "4a": 20, "4b" : 22 }
+school = {"1a":15, "1b":18, "1c":21, "2a":25, "2b":17, "2c":19, "3a":22, "3b":26, "4a":20, "4b":22}
 print(school)
 
 # 7. Создайте список и извлеките из него списка второй элемент
@@ -44,7 +45,7 @@ print(test_2 in test_3)
 
 # 9. Вывести нужные символы x = "My name is Agent Smith" print(x[?]) #y print(x[?:?:?]) #nesgt
 x = "My name is Agent Smith"
-print(x[1:22:15])
+print(x[1])
 print(x[3:17:3])
 
 # 10*. Есть массив чисел. Известно, что каждое число в этом массиве имеет пару, кроме одного: [1, 5, 2, 9, 2, 9, 1] => 5
